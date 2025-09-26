87706ff01fe2c1794def630a91c9ebbca51360a2
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-26 19:48:59 +0200

    Add recording card component and update button styles in recordings page

--- src/routes/recordings/+page.svelte
+++ src/routes/recordings/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import RecordingCard from "./recording-card.svelte";
+
 	import Nav from "$lib/components/layout/nav.svelte";
 	import { Button } from "$lib/components/ui/button/index.js";
+	import * as Card from "$lib/components/ui/card/index.js";
 	import Archive from "@lucide/svelte/icons/archive";
 	import type { PageProps } from "./$types";
 
@@ -9,7 +12,13 @@
 
 <Nav>
 	{#snippet right()}
-		<Button href="#" variant="destructive">Start new recording</Button>
+		<Button href="#" variant="outline" class="border-destructive text-destructive hover:bg-destructive hover:text-white">Start new recording</Button>
 		<Button href="/recordings/archive" variant="outline"><Archive /></Button>
 	{/snippet}
 </Nav>
+
+<div class="w-2/3 mx-auto">
+	<div class="w-1/2 mx-auto flex flex-col gap-2 mt-10">
+		<RecordingCard></RecordingCard>
+	</div>
+</div>
---
+++ src/routes/recordings/recording-card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button/index.js";
+	import * as Card from "$lib/components/ui/card/index.js";
+	import Archive from "@lucide/svelte/icons/archive";
+</script>
+
+<a href="##">
+	<Card.Root class="relative">
+		<Card.Header class="text-center">
+			<Card.Title>File name - 15:00:00 UTC</Card.Title>
+		</Card.Header>
+		<Button href="#" class="absolute -right-10 top-1/2 -translate-y-1/2" size="icon" variant="destructive"><Archive /></Button>
+	</Card.Root>
+</a>
