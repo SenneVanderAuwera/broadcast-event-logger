f977262385e3dd5c5f6386a08bd1711041550972
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-26 19:32:47 +0200

    Add button for starting a new recording in recordings page

--- src/routes/recordings/+page.svelte
+++ src/routes/recordings/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import Nav from "$lib/components/layout/nav.svelte";
 	import { Button } from "$lib/components/ui/button/index.js";
-
 	import Archive from "@lucide/svelte/icons/archive";
-
 	import type { PageProps } from "./$types";
 
 	let { data }: PageProps = $props();
@@ -11,6 +9,7 @@
 
 <Nav>
 	{#snippet right()}
+		<Button href="#" variant="destructive">Start new recording</Button>
 		<Button href="/recordings/archive" variant="outline"><Archive /></Button>
 	{/snippet}
 </Nav>
