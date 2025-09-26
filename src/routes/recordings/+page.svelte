525f826e84a5596232e574dba6f6c74a01955fac
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-26 17:26:38 +0200

    Add page load redirection and recordings page layout

---
+++ src/routes/recordings/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Nav from "$lib/components/layout/nav.svelte";
+	import type { PageProps } from "./$types";
+
+	let { data }: PageProps = $props();
+</script>
+
+<Nav>
+	{#snippet right()}
+		Hello
+	{/snippet}
+</Nav>
