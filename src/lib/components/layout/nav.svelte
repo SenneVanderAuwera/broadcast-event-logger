f7a409124726a7a0aac08a54e2f2ce85b504b414
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-26 19:34:54 +0200

    Enhance navigation layout with padding and spacing adjustments

--- src/lib/components/layout/nav.svelte
+++ src/lib/components/layout/nav.svelte
@@ -4,12 +4,12 @@
 	let { left, right }: { left?: Snippet; right?: Snippet } = $props();
 </script>
 
-<nav class="w-2/3 mx-auto flex justify-between">
-	<div class="flex-1 justify-start">
+<nav class="w-2/3 mx-auto flex justify-between p-2">
+	<div class="flex-1 justify-start gap-1">
 		{@render left?.()}
 	</div>
 
-	<div class="flex flex-1 justify-end">
+	<div class="flex flex-1 justify-end gap-1">
 		{@render right?.()}
 	</div>
 </nav>
