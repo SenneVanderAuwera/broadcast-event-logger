bc88800dfe1d4b6cb00da573dcba3250aa504560
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-27 12:27:19 +0200

    Add EventCard component and integrate it into the recordings page

---
+++ src/lib/components/events/EventCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { eventTypeColors } from "./colors";
+
+	let { type = "info" }: { type?: "info" | "warning" | "danger" } = $props();
+
+	let color = $derived(eventTypeColors[type]);
+</script>
+
+<div class={["rounded-lg shadow-md p-4 space-y-3", color]}>
+	<header class="flex justify-between">
+		<span class="text-xl font-bold"> Event title </span>
+		<span> Event date </span>
+	</header>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quas inventore tempore voluptas temporibus consequuntur? Assumenda, et repellendus error eligendi voluptatibus magnam ab nobis provident, laudantium magni maxime? Ipsam, minima.
+	</p>
+</div>
---
+++ src/routes/recordings/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import EventCard from "$lib/components/events/EventCard.svelte";
+
+	import Nav from "$lib/components/layout/nav.svelte";
+	import { Button } from "$lib/components/ui/button/index.js";
+	import type { PageProps } from "./$types";
+
+	let { data }: PageProps = $props();
+</script>
+
+<Nav>
+	{#snippet right()}
+		<Button href="#" variant="outline" class="border-destructive bg-destructive text-white animate-pulse hover:text-destructive">Stop recording</Button>
+		<Button variant="outline" href="/recordings">Back</Button>
+	{/snippet}
+</Nav>
+
+<div class="w-2/3 mx-auto">
+	<div class="w-full flex flex-col">
+		<EventCard type="warning"></EventCard>
+	</div>
+</div>
