22afa93f98c69e7ba4ae4cda28efb390711fcc96
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-27 22:25:03 +0200

    Add PocketBase integration by implementing server hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+import { POCKETBASE_URL } from "$env/static/private";
+import { json, type Handle } from "@sveltejs/kit";
+import PocketBase from "pocketbase";
+
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.pb = new PocketBase(POCKETBASE_URL);
+
+	const response = await resolve(event);
+	return response;
+};
