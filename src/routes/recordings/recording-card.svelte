3b841a0fcca5a044c03d8fab9073bfcd73dbd9f0
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-27 11:45:43 +0200

    Refactor recording card animations to use GSAP timeline for improved performance and clarity

--- src/routes/recordings/recording-card.svelte
+++ src/routes/recordings/recording-card.svelte
@@ -7,19 +7,35 @@
 	let card: HTMLDivElement;
 
 	$effect(() => {
+		const tl = gsap.timeline();
+
 		if (active) {
-			gsap.set(card, { backgroundColor: "#ffffff", border: "1px solid red", color: "red" });
-			gsap.to(card, { backgroundColor: "red", color: "white", yoyo: true, repeat: -1, duration: 1, ease: "power1.inOut" });
+			tl.to(card, { backgroundColor: "#ffffff", border: "1px solid red", color: "red" });
+			tl.to(card, { backgroundColor: "red", color: "white", yoyo: true, repeat: -1, duration: 1, ease: "power1.inOut" });
+		} else {
+			tl.set(card, { color: "black", border: "1px solid #e6e6e6", backgroundColor: "white" });
 		}
+
+		return () => {
+			gsap.killTweensOf(card);
+		};
 	});
 </script>
 
-<div class="flex items-center gap-2">
-	<a class="flex-1" href="##">
-		<div class="flex justify-between items-center p-4 rounded-md text-white shadow-lg" bind:this={card}>
-			<span>File name</span>
-			<span>00:00:00 UTC</span>
+<div class="flex items-center gap-2 group relative ms-12">
+	<div class="flex-1">
+		<a href="##">
+			<div class="flex justify-between items-center p-4 rounded-md shadow-lg" bind:this={card}>
+				<span>Recording name</span>
+				<span>File name</span>
+				<span>00:00:00 UTC</span>
+			</div>
+		</a>
+	</div>
+
+	<div class="min-w-12">
+		<div class="hidden group-hover:block">
+			<Button variant="destructive" size="icon" class="hover:cursor-pointer"><Archive /></Button>
 		</div>
-	</a>
-	<Button variant="destructive" size="icon"><Archive /></Button>
+	</div>
 </div>
