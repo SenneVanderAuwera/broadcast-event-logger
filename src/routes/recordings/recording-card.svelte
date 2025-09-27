79c8cb28bd76655b6f8ea47914f819a99c8e5ab8
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-09-27 11:56:55 +0200

    Remove GSAP animation effect from recording card and simplify styling

--- src/routes/recordings/recording-card.svelte
+++ src/routes/recordings/recording-card.svelte
@@ -1,31 +1,15 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button/index.js";
 	import Archive from "@lucide/svelte/icons/archive";
-	import gsap from "gsap";
 
 	let { active = false } = $props();
 	let card: HTMLDivElement;
-
-	$effect(() => {
-		const tl = gsap.timeline();
-
-		if (active) {
-			tl.to(card, { backgroundColor: "#ffffff", border: "1px solid red", color: "red" });
-			tl.to(card, { backgroundColor: "red", color: "white", yoyo: true, repeat: -1, duration: 1, ease: "power1.inOut" });
-		} else {
-			tl.set(card, { color: "black", border: "1px solid #e6e6e6", backgroundColor: "white" });
-		}
-
-		return () => {
-			gsap.killTweensOf(card);
-		};
-	});
 </script>
 
 <div class="flex items-center gap-2 group relative ms-12">
 	<div class="flex-1">
 		<a href="##">
-			<div class="flex justify-between items-center p-4 rounded-md shadow-lg" bind:this={card}>
+			<div class={["flex justify-between items-center p-4 rounded-md shadow-lg border", { "animate-pulse bg-destructive text-white": active }]}>
 				<span>Recording name</span>
 				<span>File name</span>
 				<span>00:00:00 UTC</span>
