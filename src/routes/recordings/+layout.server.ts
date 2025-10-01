598d34081ff2cbf21cf6ce4de66aa42099cde3ca
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-10-01 20:18:02 +0200

    Refactor activeRecording return logic to ensure consistent null handling

--- src/routes/recordings/+layout.server.ts
+++ src/routes/recordings/+layout.server.ts
@@ -1,11 +1,12 @@
+import { ClientResponseError } from "pocketbase";
 import type { LayoutServerLoad } from "./$types";
 
 export const load = (async ({ locals }) => {
 	try {
 		const activeRecordingResult = await locals.pb.collection("recording").getFirstListItem("stop=''", { fields: "id" });
-		return { activeRecording: activeRecordingResult || null };
+		return { activeRecording: activeRecordingResult };
 	} catch (err) {
-		console.error(err);
-		return {};
+		if (err instanceof ClientResponseError && err.status !== 404) console.error(err);
+		return { activeRecording: null };
 	}
 }) satisfies LayoutServerLoad;
