79af9f197a8eee4ab1ba81c422f70e0b6af91b15
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-10-01 20:36:40 +0200

    Refactor EventCard to display event title and timestamp dynamically; update message content to use data from props

--- src/lib/components/events/EventCard.svelte
+++ src/lib/components/events/EventCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { EventRecord } from "$lib/pocketbase/types";
+	import { DateTime } from "luxon";
 	import { eventTypeColors } from "./colors";
 
 	let { data }: { data: EventRecord } = $props();
@@ -13,14 +14,13 @@
 	<div class="basis-48 text-center">timestamp</div>
 
 	<div class={["rounded-lg p-4 space-y-3 flex-1 print:p-1", color]}>
-		<header class="flex justify-between">
-			<span class="text-xl font-bold"> Event title </span>
-			<span> Event date </span>
+		<header class="flex justify-between mb-0">
+			<span class="text-xl font-bold"> {data.title} </span>
+			<span> {DateTime.fromSQL(data.timestamp).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} </span>
 		</header>
 
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quas inventore tempore voluptas temporibus consequuntur? Assumenda, et repellendus error eligendi voluptatibus magnam ab nobis provident, laudantium magni maxime? Ipsam,
-			minima.
+			{data.message}
 		</p>
 	</div>
 </div>
