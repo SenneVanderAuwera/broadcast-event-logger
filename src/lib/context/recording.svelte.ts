8e8d7440f8237cce7a08fe057d76b732dc80b620
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-10-05 22:05:20 +0200

    Refactor Recording class to include subscription management and initialize records array

--- src/lib/context/recording.svelte.ts
+++ src/lib/context/recording.svelte.ts
@@ -1,13 +1,23 @@
-import type { RecordingRecord, RecordingResponse } from "$lib/pocketbase/types";
-import { getContext, setContext } from "svelte";
+import { pb } from "$lib/pocketbase";
+import type { RecordingResponse } from "$lib/pocketbase/types";
+import { getContext, onMount, setContext } from "svelte";
 
 class Recording {
 	active = $state(false);
 	id = $state("");
+	records: RecordingResponse[] = $state([]);
 
-	constructor() {}
+	constructor() {
+		onMount(() => {
+			pb.collection("recording").subscribe("*", (e) => {});
 
-	init(record: RecordingRecord) {
+			return () => {
+				pb.collection("recording").unsubscribe("*");
+			};
+		});
+	}
+
+	init(record: RecordingResponse) {
 		if (!record) this.clear();
 
 		if (record?.stop === "") {
@@ -15,6 +25,22 @@ class Recording {
 			this.id = record.id;
 		} else {
 			this.clear();
+		}
+	}
+
+	async start() {
+		try {
+			const recordingResponse = await fetch("/api/recording/start", { method: "POST", body: "{}" });
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	async stop() {
+		try {
+			const recordingResponse = await fetch("/api/recording/stop", { method: "POST", body: "{}" });
+		} catch (err) {
+			throw err;
 		}
 	}
 
