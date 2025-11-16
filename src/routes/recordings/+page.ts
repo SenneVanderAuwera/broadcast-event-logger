757ae105298a4d87fa6a3510117160ed95726b15
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2025-11-16 21:46:35 +0100

    Remove load function implementation for non-archived recordings

--- src/routes/recordings/+page.ts
+++ src/routes/recordings/+page.ts
@@ -2,13 +2,4 @@ import { ClientResponseError } from "pocketbase";
 import type { PageLoad } from "./$types";
 import { pb } from "$lib/pocketbase";
 
-export const load = (async ({ fetch, depends }) => {
-	depends("recordings:non-archived");
-	try {
-		const recordingsResult = await pb.collection("recording").getFullList({ filter: "archived=false", sort: "-created", fetch });
-		return { recordings: recordingsResult };
-	} catch (err) {
-		if (err instanceof ClientResponseError && err.status !== 404) console.error(err);
-		return { recordings: [] };
-	}
-}) satisfies PageLoad;
+export const load = (async ({ fetch, depends }) => {}) satisfies PageLoad;
