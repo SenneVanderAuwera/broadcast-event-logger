f9034f2cd267acf8a72f262cdf67f8f606afd7bc
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2026-03-17 11:55:01 +0100

    feat: add Toaster component for enhanced notifications in layout

--- src/routes/recordings/+layout.svelte
+++ src/routes/recordings/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { RecordingController, setRecordingControllerCtx } from "$lib/context/recordingController.svelte";
+	import { Toaster } from "$lib/components/ui/sonner/index.js";
 	import type { LayoutProps } from "./$types";
 
 	let { data, children }: LayoutProps = $props();
 
 	setRecordingControllerCtx(new RecordingController());
 </script>
+
+<Toaster richColors />
 
 {@render children()}
