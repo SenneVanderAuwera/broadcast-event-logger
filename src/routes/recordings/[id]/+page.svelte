5df1a1768e6348c9542bb12ba6ddb71230656ab9
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2026-03-17 12:31:56 +0100

    feat: implement editable recording name and improve event handling

--- src/routes/recordings/[id]/+page.svelte
+++ src/routes/recordings/[id]/+page.svelte
@@ -18,15 +18,32 @@
 	import TriangleAlert from "@lucide/svelte/icons/triangle-alert";
 	import { DateTime } from "luxon";
 	import { onMount } from "svelte";
+	import HoverInput from "$lib/components/events/HoverInput.svelte";
+	import { invalidateAll } from "$app/navigation";
+	import { toast } from "svelte-sonner";
 
 	let { data }: PageProps = $props();
 
 	const recordingController = getRecordingControllerCtx();
 
+	// svelte-ignore state_referenced_locally
+	let recording = $state(data.recording);
+
 	$effect(() => {
+		recording = data.recording;
 		recordingController.recordings = [data.recording];
 		recordingController.recordingEvents = data.events;
 	});
+
+	async function handleRecordingNameChange() {
+		try {
+			await pb.collection(Collections.Recordings).update(recording.id, { ...recording });
+		} catch (err) {
+			toast.error("Failed to update event title");
+			console.error(err);
+			invalidateAll();
+		}
+	}
 
 	onMount(() => {
 		pb.collection(Collections.RecordingEvents).subscribe<RecordingEventsResponse>("*", ({ action, record }) => {
@@ -63,13 +80,13 @@
 	<div class="w-full flex flex-col gap-2">
 		<RecordingCard>
 			{#snippet left()}
-				<span class="text-xl font-bold"> {data.recording.recording_name} </span>
+				<HoverInput className={"bg-transparent! border-0 text-lg! font-bold px-1 hover:bg-white/20! w-44 focus-visible:ring-0"} bind:value={recording.recording_name} onchange={handleRecordingNameChange} />
 			{/snippet}
 			{#snippet center()}
-				<span class="text-xl font-bold"> {data.recording.filename} </span>
+				<span class="text-xl font-bold"> {recording.filename} </span>
 			{/snippet}
 			{#snippet right()}
-				<span> {DateTime.fromSQL(data.recording.start).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} </span>
+				<span> {DateTime.fromSQL(recording.start).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} </span>
 			{/snippet}
 		</RecordingCard>
 
@@ -87,9 +104,9 @@
 			<div class="flex">
 				<div class="basis-48"></div>
 				<div class="flex-1">
-					<Button onclick={() => createNewEvent(data.recording.id, "info", DateTime.now())} size="icon" class={[eventStyles.default.info, eventStyles.hover.info, "cursor-pointer"]}><Info /></Button>
-					<Button onclick={() => createNewEvent(data.recording.id, "warning", DateTime.now())} size="icon" class={[eventStyles.default.warning, eventStyles.hover.warning, "cursor-pointer"]}><TriangleAlert /></Button>
-					<Button onclick={() => createNewEvent(data.recording.id, "error", DateTime.now())} size="icon" class={[eventStyles.default.error, eventStyles.hover.error, "cursor-pointer"]}><Ban /></Button>
+					<Button onclick={() => createNewEvent(recording.id, "info", DateTime.now())} size="icon" class={[eventStyles.default.info, eventStyles.hover.info, "cursor-pointer"]}><Info /></Button>
+					<Button onclick={() => createNewEvent(recording.id, "warning", DateTime.now())} size="icon" class={[eventStyles.default.warning, eventStyles.hover.warning, "cursor-pointer"]}><TriangleAlert /></Button>
+					<Button onclick={() => createNewEvent(recording.id, "error", DateTime.now())} size="icon" class={[eventStyles.default.error, eventStyles.hover.error, "cursor-pointer"]}><Ban /></Button>
 				</div>
 			</div>
 		{:else}
@@ -98,10 +115,10 @@
 					<span class="text-xl font-bold"> Recording end </span>
 				{/snippet}
 				{#snippet center()}
-					<span class="text-xl font-bold"> {getRelativeDuration(DateTime.fromSQL(data.recording.start), DateTime.fromSQL(data.recording.stop)).toFormat("hh:mm:ss")}</span>
+					<span class="text-xl font-bold"> {getRelativeDuration(DateTime.fromSQL(recording.start), DateTime.fromSQL(recording.stop)).toFormat("hh:mm:ss")}</span>
 				{/snippet}
 				{#snippet right()}
-					<span> {DateTime.fromSQL(data.recording.stop).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} </span>
+					<span> {DateTime.fromSQL(recording.stop).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)} </span>
 				{/snippet}
 			</RecordingCard>
 		{/if}
