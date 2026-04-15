43b92ddae3fa28f5e9f30572fd2563115f1b91d4
Senne Van der Auwera <senne.vanderauwera@telenet.be>
2026-04-15 22:42:16 +0200

    feat: integrate Label component for checkbox selection and archive button visibility

--- src/routes/recordings/+page.svelte
+++ src/routes/recordings/+page.svelte
@@ -5,6 +5,7 @@
 	import Nav from "$lib/components/layout/nav.svelte";
 	import { Button } from "$lib/components/ui/button/index.js";
 	import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+	import { Label } from "$lib/components/ui/label/index.js";
 	import { getRecordingControllerCtx } from "$lib/context/recordingController.svelte";
 	import { pb } from "$lib/pocketbase";
 	import { Collections } from "$lib/pocketbase/types";
@@ -56,12 +57,6 @@
 
 <Nav>
 	{#snippet right()}
-		{#if hasSelection}
-			<Button variant="destructive" class="hover:bg-destructive/80 hover:cursor-pointer" onclick={archiveSelected}>
-				<Archive />
-				Archive selected
-			</Button>
-		{/if}
 		{#if recordingController.state.active}
 			<Button href={`/recordings/${recordingController.state.record?.id}`} variant="destructive" class="hover:bg-destructive/80">Go to active recording</Button>
 		{:else}
@@ -73,9 +68,18 @@
 
 <div class="w-2/3 mx-auto">
 	{#if data.recordings.length > 0}
-		<div class="flex items-center gap-2 mt-10 mb-2 px-1">
-			<Checkbox checked={allSelected} indeterminate={someSelected} onCheckedChange={toggleSelectAll} aria-label="Select all recordings" />
-			<span class="text-sm text-muted-foreground">Select all</span>
+		<div class="flex items-center gap-2 mb-2 px-1 h-8">
+			<Label>
+				<Checkbox checked={allSelected} indeterminate={someSelected} onCheckedChange={toggleSelectAll} aria-label="Select all recordings" />
+
+				<span class="text-sm text-muted-foreground">Select all</span>
+			</Label>
+			{#if hasSelection}
+				<Button variant="destructive" size="sm" class="hover:bg-destructive/80 hover:cursor-pointer" onclick={archiveSelected}>
+					<Archive />
+					Archive selected
+				</Button>
+			{/if}
 		</div>
 	{/if}
 	<div class="w-full mx-auto flex flex-col gap-2">
